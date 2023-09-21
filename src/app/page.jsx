238434b957b23839f8158532f28f5443c5fadf95
Haider Ali <hassanhussaian630@gmail.com>
2023-09-20 23:46:32 -0700

    commit

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+ return(
+  <div>
+    This is Airbnb clone using latest next 13 app router
+  </div>
+ )
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import Container from "../Container";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed bg-white z-10 shadow-sm w-full">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+            <div className="flex flex-row item-center justify-center gap-3  md:gap-0">
+
+            </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
